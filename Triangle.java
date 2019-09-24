ddbc19064a98df3ecbdcb401f99272ab31b85590
Marcus Appelros <marcus.appelros@gmail.com>
2019-09-24 11:33:37 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class Triangle{
 	int x;
 	int y;
 	int pixX;
 	int pixY;
 	int player;
+	int prevPlayer;
+	ArrayList<Triangle> captured=new ArrayList<Triangle>();
 	public Triangle(int x,int y,int pixX,int pixY){
 		this.x=x;
 		this.y=y;
