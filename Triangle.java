6db919ba2301afab63def95adb449e2697a2a565
Marcus Appelros <marcus.appelros@gmail.com>
2019-09-24 09:22:39 +0200

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle{
+	int x;
+	int y;
+	int pixX;
+	int pixY;
+	int player;
+	public Triangle(int x,int y,int pixX,int pixY){
+		this.x=x;
+		this.y=y;
+		this.pixX=pixX;
+		this.pixY=pixY;
+		this.player=0;
+	}
+}
